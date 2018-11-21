26549407dbfd75e860e2db8a4fea8f796ede199d
kit tumbagahan <kittumbagahan@gmail.com>
2018-11-21 16:33:57 +0800

    Network Save Activity
    Network Save Activity

---
+++ Assets/Scripts/_Network/NetworkData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NetworkData {
+
+    // for StudentActivityModel
+    public int ID;
+    public int sectionId;
+    public int studentId;
+    public int bookId;
+    public int activityId;
+    public string grade;
+    public int playCount;    
+}
--- Assets/Scripts/_parent_control/StudentLogIn.cs
+++ Assets/Scripts/_parent_control/StudentLogIn.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class StudentLogIn : MonoBehaviour {
 
+    [SerializeField]
+    GameObject authentication, sectionSelection;
+
 	public void LogIn()
     {
         UserRestrictionController.ins.Restrict(1);
+
+        // kit, gameobjects
+        sectionSelection.gameObject.SetActive(true);
+        authentication.gameObject.SetActive(false);
     }
 }
