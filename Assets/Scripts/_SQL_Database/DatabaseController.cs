f176c07a1151be9d0e7cc122b8b4cafa5c85389f
palabaydev <palabaydev@gmail.com>
2018-11-23 17:39:45 +0800

    backupdatabase design

---
+++ Assets/Scripts/_SQL_Database/DatabaseController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DatabaseController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+      CopySomething ();
+	}
+
+   public void CopySomething()
+   {
+      //FileUtil.CopyFileOrDirectory ("sourcepath/YourFileOrFolder", "destpath/YourFileOrFolder");
+      FileUtil.CopyFileOrDirectory ("Assets/StreamingAssets/tempDatabase.db", "Assets/StreamingAssets/copy_tempDatabase.db");
+   }
+}
