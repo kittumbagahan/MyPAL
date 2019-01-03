c99aef0789fc398b4e2de3a909d97b23457212ea
nhono99 <ronaldabellano@gmail.com>
2019-01-03 20:04:41 +0800

    Hidden Launcher button

---
+++ Assets/Scripts/_Launcher/HiddenLauncherButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class HiddenLauncherButton : MonoBehaviour, IPointerClickHandler
+{
+
+    int clicks = 0;
+
+    bool clickActive = false;
+
+    void ResetClicks()
+    {
+        clicks = 0;
+        clickActive = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!clickActive)
+        {
+            clickActive = true;
+            Invoke("ResetClicks", 3);
+        }
+        clicks++;
+        print("hidden clicks " + clicks.ToString());
+        if (clicks >= 5)
+        {
+            SceneManager.LoadScene("Launcher");
+        }
+    }
+}
