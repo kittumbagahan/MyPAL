22d704f75ae35d0f4b0f3c15a7ed4a69d6572c21
palabaydev <palabaydev@gmail.com>
2019-04-10 17:59:36 +0800

    Integrated asset bundle browser
    Integrated asset bundle browser

--- Assets/Scripts/_Assetbundle/BundleCreator.cs
+++ Assets/Scripts/_Assetbundle/BundleCreator.cs
@@ -6,7 +6,7 @@ public class BundleCreator{
     [MenuItem("Assets/Build AssetBundles")]
     static void BuildAllAssetBundles()
     {
-        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.Android);
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.Android);	    
     }
 
 	
